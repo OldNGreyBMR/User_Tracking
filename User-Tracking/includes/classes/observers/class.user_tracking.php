1deed4920d57ade843e769332c50f30d967e1a1a
mc12345678 <mc12345678@users.noreply.github.com>
2021-01-31 03:57:47 -0500

    Address missing key gID on line 101 if missing HEADING_TITLE
    Fixes #7

--- User-Tracking/includes/classes/observers/class.user_tracking.php
+++ User-Tracking/includes/classes/observers/class.user_tracking.php
@@ -98,7 +98,7 @@ class user_tracking extends base
         } else {
             $page_desc = defined('HEADING_TITLE') ? HEADING_TITLE : (IS_ADMIN_FLAG !== true ? NAVBAR_TITLE : '');
             if (IS_ADMIN_FLAG === true && (empty($page_desc) || !defined('HEADING_TITLE'))) {
-                $page_desc_values = $db->Execute("select configuration_group_title from " . TABLE_CONFIGURATION_GROUP . " where configuration_group_id = " . (int)$_GET['gID']);
+                $page_desc_values = $db->Execute("select configuration_group_title from " . TABLE_CONFIGURATION_GROUP . " where configuration_group_id = " . (int)(isset($_GET['gID']) ? $_GET['gID'] : 0));
                 $page_desc        = (!$page_desc_values->EOF) ? $page_desc_values->fields['configuration_group_title'] : '';
             }
         }
